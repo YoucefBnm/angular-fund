17b1448c51fc836c2f95c40ad140f769aa6c0f21
Josef Bnm <ssefbnm@gmail.com>
2020-08-11 20:11:40 +0100

    first commit

---
+++ src/app/event-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-app',
+  template: `
+    <h2>Youcef Bnm</h2>
+    <img src="/assets/images/basic-shield.png">
+  `,
+})
+export class EventAppComponent {
+  title = 'event app';
+}
