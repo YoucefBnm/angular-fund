b2f9ce56ff324097b8691fdaff8f99d510412174
Josef Bnm <ssefbnm@gmail.com>
2020-08-13 19:59:58 +0100

    add more events to API

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,21 +1,30 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="mar-left">{{event.location.city}}, {{event.location.country}}</span>
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
       </div>
+      <div>Price: \${{event?.price}}</div>
+      <div [hidden]="!event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="mar-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div [hidden]="!event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
     </div>
   `,
   styles:[
-    `.mar-left { margin-left: 10px; }
+    `
+    .thumbnail { min-height: 210px; }
+    .mar-left { margin-left: 10px; }
      .well div { color: #bbb; }
     `
   ]
