f30de04eac483343626cb0e9576611e3cea09028
Josef Bnm <ssefbnm@gmail.com>
2020-08-23 15:51:13 +0100

    organizing imports with barrels

--- src/app/events/event-list-resolver.service.ts
+++ src/app/events/event-list-resolver.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Resolve } from '@angular/router'
 import { map } from 'rxjs/operators'
-import { EventService } from './event.service'
+import { EventService } from './shared/event.service'
 
 @Injectable()
 export class EventListResolver implements Resolve<any> {
