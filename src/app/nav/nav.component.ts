926fba8499b68ed98a4c1974008bb89817f1084f
Josef Bnm <ssefbnm@gmail.com>
2020-08-28 15:28:27 +0100

    add authentication user

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../user/auth.service'
 
 @Component({
   selector: 'nav',
@@ -12,5 +13,7 @@ import { Component } from '@angular/core';
 })
 
 export class NavComponent {
+  constructor(private auth:AuthService) {
 
+  }
 }
